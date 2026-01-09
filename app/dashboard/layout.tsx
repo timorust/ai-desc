c16e4c98b72b654c56deb1ccccbd054f2ea48cf2
Tamir Rustamov <timorust1@gmail.com>
2026-01-09 11:13:24 +0200

    plan correct

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId, has } = await auth();
+
+  if (!userId) redirect("/");
+  const hasPaidPlan =
+    (await has({ plan: "for_serious_newsletter_creators" })) ||
+    (await has({ plan: "starter" }));
+  console.log("Has paid plan:=>", hasPaidPlan);
+  if (!hasPaidPlan) redirect("/#pricing");
+
+  return <div className="min-h-screen flex flex-col">{children}</div>;
+}
